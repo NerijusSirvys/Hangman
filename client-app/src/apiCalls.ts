458040068865cadb7371d94c4de79155e9eea712
nerijus.sirvys <nerijus.sirvys@gmail.com>
2021-07-07 07:24:14 +0100

    Added loading message before new level is rendered

--- client-app/src/apiCalls.ts
+++ client-app/src/apiCalls.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
-import { Dispatch } from "react";
-import { loadNewLevel } from "./actions/levelActions";
 import { Level } from "./types/interfaces/Level";
 import { Player } from "./types/interfaces/Player";
 
-export const LoadLevel = (dispatch: Dispatch<any>, url: string) =>
-  axios.get<Level>(url).then((response) => {
-    dispatch(loadNewLevel(response.data));
+export const getLevel = (url: string) => {
+  return axios.get<Level>(url).then((response) => {
+    return response.data;
   });
+};
 
 export const UpdatePlayer = (player: Player) => {
   // TODO: push player to API
-  alert("Player saved");
 };
